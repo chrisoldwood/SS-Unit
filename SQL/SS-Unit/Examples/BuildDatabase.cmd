c0a92d822ad3609b43590ba5e21d46aec51b0f7d
Admin <admin@chrisoldwood.com>
2011-06-14 22:28:29 +0000

    Implemented scripts properly and made them use file based lists.

--- SQL/SS-Unit/Examples/BuildDatabase.cmd
+++ SQL/SS-Unit/Examples/BuildDatabase.cmd
@@ -1,18 +1,59 @@
 @echo off
+rem ************************************************************
+rem
+rem Build the Examples database and install the SS-Unit
+rem framework into it.
+rem
+rem ************************************************************
 
+:handle_help_request
+if /i "%1" == "-?"     call :usage & exit /b 0
+if /i "%1" == "--help" call :usage & exit /b 0
+
+:check_mandatory_args
+if /i "%1" == "" call :usage & exit /b 1
+
+:set_vars
 setlocal
+setlocal enabledelayedexpansion
 set server=%1
 set database=SSUnit_Examples
 
-echo Creating database...
-sqlcmd -E -S %server% -d master -Q "drop database %database%;"
-sqlcmd -E -S %server% -d master -Q "create database %database%;"
+echo.
+echo ----------------------------------------
+echo Creating '%database%' database
+echo ----------------------------------------
+echo.
 
-echo Creating 'dbo' objects...
+sqlcmd -E -S %server% -d master -i CreateDatabase.dbo.sql
+
+for /f "delims=" %%f in (object-scripts.txt) do (
+	echo %%f
+	sqlcmd -E -S %server% -d %database% -b -i "%%f"
+	if errorlevel 1 (
+		echo ERROR: Failed to execute SQL script [!ERRORLEVEL!]
+		exit /b 1
+	)
+)
 
 pushd ..\Framework
 call Install %server% %database%
 popd
+if errorlevel 1 (
+	echo ERROR: Failed to install SS-Unit [!ERRORLEVEL!]
+	exit /b 1
+)
 
-echo Creating 'test' objects...
-sqlcmd -E -S %server% -d %database% -i CreateSchema.test.sql
+:success
+exit /b 0
+
+rem ************************************************************
+rem Functions
+rem ************************************************************
+
+:usage
+echo.
+echo Usage: %~n0 ^<db server^>
+echo.
+echo e.g.   %~n0 .\SQLEXPRESS
+goto :eof
--- SQL/SS-Unit/Examples/RunTests.cmd
+++ SQL/SS-Unit/Examples/RunTests.cmd
@@ -1,9 +1,66 @@
 @echo off
+rem ************************************************************
+rem
+rem Run the Examples database unit tests.
+rem
+rem ************************************************************
 
+:handle_help_request
+if /i "%1" == "-?"     call :usage & exit /b 0
+if /i "%1" == "--help" call :usage & exit /b 0
+
+:check_mandatory_args
+if /i "%1" == "" call :usage & exit /b 1
+
+:set_vars
 setlocal
+setlocal enabledelayedexpansion
 set server=%1
 set database=SSUnit_Examples
+set failed=false
 
-echo Running tests...
+:run_tests
 echo.
-sqlcmd -E -S %server% -d %database% -i BasicAsserts.test.sql
+echo ----------------------------------------
+echo Running unit tests
+echo ----------------------------------------
+echo.
+
+for /f "delims=" %%f in (test-scripts.txt) do (
+	echo.
+	echo ========================================
+	echo %%f
+	echo ========================================
+	echo.
+	sqlcmd -E -S %server% -d %database% -b -i "%%f"
+	if errorlevel 1 set failed=true
+)
+
+if /i "%failed%" == "true" goto failed
+
+:success
+echo.
+echo ----------------------------------------
+echo --         Test run succeeded         --
+echo ----------------------------------------
+echo.
+exit /b 0
+
+:failed
+echo.
+echo ****************************************
+echo *****        TEST RUN FAILED       *****
+echo ****************************************
+echo.
+exit /b 1
+
+rem ************************************************************
+rem Functions
+rem ************************************************************
+
+:usage
+echo.
+echo Usage: %~n0 ^<db server^>
+echo.
+echo e.g.   %~n0 .\SQLEXPRESS
+goto :eof
