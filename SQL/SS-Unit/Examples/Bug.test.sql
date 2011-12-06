1f4152fbb2ce0fef89d5b394d26a087696152387
Admin <admin@chrisoldwood.com>
2011-12-06 22:10:25 +0000

    Updated for 0.9 release.

--- SQL/SS-Unit/Examples/Bug.test.sql
+++ SQL/SS-Unit/Examples/Bug.test.sql
@@ -4,54 +4,35 @@
  * \author Chris Oldwood
  */
 
-create procedure test.InsertInvalidUser
+create procedure test._@FixtureSetUp@_$Bug$_
 as
-	declare @invalidUserId int;
-	set		@invalidUserId = -1;
-
-	insert into dbo.Bug(Summary, SubmitUserId)
-	             values('test',  @invalidUserId);
+	create table test.BugResults
+	(
+		BugId	int				not null,
+		Summary	varchar(256)	not null,
+	);
 go
 
-create procedure test._@Test@_Bug_ShouldThrow_WhenInvalidUserSpecified
+create procedure test._@FixtureTearDown@_$Bug$_
 as
-	exec ssunit.AssertThrew '%foreign%key%constraint%SubmitUserId%', 'test.InsertInvalidUser';
+	drop table test.BugResults;
 go
 
-/*******************************************************************************
- * Helper procedures.
- * NB: These have to be manually dropped at the moment.
- */
-
-if (object_id('test.InsertUser') is not null)
-	drop procedure test.InsertUser
-go
-
-create procedure test.InsertUser
-(
-	@userId		int,
-	@loginName	varchar(50)
-)
+create procedure test._@TestSetUp@_$Bug$_
 as
+	declare @userId int = 99;
+	declare @loginName varchar(50) = 'test';
+
 	set identity_insert dbo.SystemUser on;
 
 	insert into dbo.SystemUser(UserId, LoginName, FirstName, LastName)
 	                   values (@userId, @loginName, 'test', 'test');
 
 	set identity_insert dbo.SystemUser off;
-go
 
-if (object_id('test.InsertBug') is not null)
-	drop procedure test.InsertBug
-go
+	declare @bugId int = 42;
+	declare @summary varchar(50) = 'test summary';
 
-create procedure test.InsertBug
-(
-	@bugId		int,
-	@summary	varchar(50),
-	@userId		int
-)
-as
 	set identity_insert dbo.Bug on;
 
 	insert into dbo.Bug(BugId, Summary, SubmitUserId)
@@ -60,46 +41,39 @@ as
 	set identity_insert dbo.Bug off;
 go
 
-/*
-***************************************************************************** */
-
-create procedure test._@Test@_Bug_FindBugsSubmittedByUser_ShouldReturnBugIdAndSummary
+create procedure test._@TestTearDown@_$Bug$_
 as
-	declare @userId int;
-	set		@userId = 99;
-
-	declare @bugId int;
-	set		@bugId = 42;
-
-	declare @summary varchar(50);
-	set		@summary = 'test summary';
-
 	delete from dbo.Bug;
 	delete from dbo.SystemUser;
+go
 
-	exec test.InsertUser @userId, 'test';
-	exec test.InsertBug @bugId, @summary, @userId;
+create procedure test.InsertInvalidUser
+as
+	declare @invalidUserId int = -1;
 
-	create table #results
-	(
-		BugId	int				not null,
-		Summary	varchar(256)	not null,
-	);
+	insert into dbo.Bug(Summary, SubmitUserId)
+	             values('test',  @invalidUserId);
+go
 
-	insert into #results
+create procedure test._@Test@_$Bug$_InsertingBug_ShouldThrow_WhenInvalidUserSpecified
+as
+	exec ssunit.AssertThrew '%foreign%key%constraint%SubmitUserId%', 'test.InsertInvalidUser';
+go
+
+create procedure test._@Test@_$Bug$_FindBugsSubmittedByUser_ShouldReturnBugIdAndSummary
+as
+	declare @userId int = 99;
+	declare @bugId int = 42;
+	declare @summary varchar(50) = 'test summary';
+
+	insert into BugResults
 	exec dbo.Bug_FindBugsSubmittedByUser @userId;
 
-	if exists (	select	1
-				from	#results
-				where	BugId = @bugId
-				and		Summary = @summary )
-	begin
-		exec ssunit.AssertPass;
-	end
-	else	
-	begin
-		exec ssunit.AssertFail 'bug not returned';
-	end
+	declare @count int;
+	select	@count = count(*) from BugResults
+	where	BugId = @bugId and Summary = @summary;
+
+	exec ssunit.AssertIntegerEqualTo 1, @count;
 go
 
 exec ssunit.RunTests;
