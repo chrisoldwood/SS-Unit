1cfe99e2bb88aee13737b3634fe3bc463805ee0a
Admin <admin@chrisoldwood.com>
2012-03-27 20:59:18 +0000

    Initial version.

---
+++ SQL/SS-Unit/Framework/TestHelper_Delete.ssunit.sql
@@ -0,0 +1,51 @@
+/**
+ * \file   TestHelper_Delete.ssunit.sql.ssunit.sql
+ * \brief  The TestHelper_Delete stored procedure.
+ * \author Chris Oldwood
+ */
+
+if (object_id('ssunit.TestHelper_Delete') is not null)
+	drop procedure ssunit.TestHelper_Delete;
+go
+
+/**
+ * Deletes the test helper procedures.
+ */
+
+create procedure ssunit.TestHelper_Delete
+(
+	@schemaName		ssunit.SchemaName		--!< The schema used for the tests.
+)
+as
+	declare @testSchemaId int;
+
+	select	@testSchemaId = schema_id
+	from	sys.schemas s
+	where	s.name = @schemaName;
+
+	declare ProcedureCursor cursor local fast_forward
+	for
+	select	(@schemaName + '.' + p.name) as ProcedureName
+	from	sys.procedures p
+	where	p.schema_id = @testSchemaId
+	and		p.name like '[_]@Helper@[_]%';
+
+	open ProcedureCursor;
+
+	while (1 = 1)
+	begin
+		declare @procedure ssunit.ProcedureName;
+
+		fetch	next
+		from	ProcedureCursor
+		into	@procedure;
+
+		if (@@fetch_status <> 0)
+			break;
+
+		exec('drop procedure ' + @procedure);
+	end
+
+	close ProcedureCursor;
+	deallocate ProcedureCursor;
+go
